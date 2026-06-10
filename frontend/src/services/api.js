82604d5ae0e7e4647a2ffed96513d407f6fca37e
sjonhkan48 <void3467@proton.me>
2026-06-10 14:03:36 +0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,33 +1,74 @@
 import axios from "axios"
 
-export const API_URL = "https://demo-game-3.onrender.com"
 
-export async function getScore(playerId) {
-  const res = await axios.get(
-    `${API_URL}/api/score/${playerId}`
-  )
-  return res.data
+export const API_URL =
+"https://demo-game-3.onrender.com"
+
+
+
+export async function getScore(playerId){
+
+
+const res =
+await axios.get(
+`${API_URL}/api/score/${playerId}`
+)
+
+
+return res.data
+
 }
 
-export async function changeScore(playerId, delta) {
-  const res = await axios.post(
-    `${API_URL}/api/score/${playerId}`,
-    { delta }
-  )
-  return res.data
+
+
+
+
+export async function createBet(data){
+
+
+const res =
+await axios.post(
+`${API_URL}/api/bet`,
+data
+)
+
+
+return res.data
+
+
 }
 
-export async function createBet(data) {
-  const res = await axios.post(
-    `${API_URL}/api/bet`,
-    data
-  )
-  return res.data
+
+
+
+
+export async function getBets(playerId){
+
+
+const res =
+await axios.get(
+`${API_URL}/api/bets/${playerId}`
+)
+
+
+return res.data
+
+
 }
 
-export async function getBets() {
-  const res = await axios.get(
-    `${API_URL}/api/bets`
-  )
-  return res.data
+
+
+
+export async function getGame(){
+
+
+const res =
+await axios.get(
+`${API_URL}/api/result`
+)
+
+
+return res.data
+
+
 }
