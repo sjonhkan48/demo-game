ff07931144c1d66990a54e18e7546676c992a791
sjonhkan48 <void3467@proton.me>
2026-06-10 14:56:10 +0800

    Update game.js

--- backend/services/game.js
+++ backend/services/game.js
@@ -1,112 +1,19 @@
-
 const game = {
-
-
-result:"等待开奖",
-
-
-bettingOpen:true,
-
-
-time:20
-
-
-
-}
-
-
-
-
-
-// 每秒运行
-
-setInterval(()=>{
-
-
-if(game.bettingOpen){
-
-
-game.time--
-
-
-
-if(game.time<=0){
-
-
-game.bettingOpen=false
-
-
-game.time=0
-
-
-game.result="等待开奖"
-
-
-
-}
-
-
-
-}
-
-
-
-},1000)
-
-
-
-
-
-// 后台开奖
-
-function openResult(value){
-
-
-game.result=value
-
-
-game.bettingOpen=false
-
-
-game.time=0
-
-
-}
-
-
-
-
-
-// 下一轮
-
-function nextRound(){
-
-
-game.result="等待开奖"
-
-
-game.bettingOpen=true
-
-
-game.time=20
-
-
-}
-
-
-
-
-
-module.exports={
-
-
-game,
-
-
-openResult,
-
-
-nextRound
-
-
-}
+  result: "等待开奖",
+  bettingOpen: true,
+  time: 20,
+};
+
+// 每秒倒计时
+setInterval(() => {
+  if (game.bettingOpen) {
+    game.time--;
+    if (game.time <= 0) {
+      game.bettingOpen = false;
+      game.time = 0;
+      game.result = "等待开奖";
+    }
+  }
+}, 1000);
+
+module.exports = { game };
