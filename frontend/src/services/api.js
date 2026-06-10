d029c74717e8d3feafe694850e4e1e9ffd3d5643
sjonhkan48 <void3467@proton.me>
2026-06-10 12:53:31 +0800

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+export const API_URL = "http://localhost:3000"
+
+export async function getScore(playerId) {
+  const res = await axios.get(
+    `${API_URL}/api/score/${playerId}`
+  )
+  return res.data
+}
+
+export async function changeScore(playerId, delta) {
+  const res = await axios.post(
+    `${API_URL}/api/score/${playerId}`,
+    { delta }
+  )
+  return res.data
+}
+
+export async function createBet(data) {
+  const res = await axios.post(
+    `${API_URL}/api/bet`,
+    data
+  )
+  return res.data
+}
+
+export async function getBets() {
+  const res = await axios.get(
+    `${API_URL}/api/bets`
+  )
+  return res.data
+}
