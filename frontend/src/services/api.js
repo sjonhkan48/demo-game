7ea64007332dc8567b9204c822a9b33eed2bf51b
sjonhkan48 <void3467@proton.me>
2026-06-10 13:46:19 +0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = "http://localhost:3000"
+export const API_URL = "https://demo-game-3.onrender.com"
 
 export async function getScore(playerId) {
   const res = await axios.get(
