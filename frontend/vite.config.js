cf9bed6b1ba248bd8a669ac795d69180ea75be6f
sjonhkan48 <void3467@proton.me>
2026-06-10 16:02:47 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'frontend/dist'
+  }
 })
