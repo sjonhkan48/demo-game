045fd2c9b7d453c3f2131c15f1a41ef3438d85ed
sjonhkan48 <void3467@proton.me>
2026-06-11 13:59:44 +0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,59 +1,51 @@
 import axios from "axios";
 
-export const API_URL = "https://demo-game-2.onrender.com";
 
-// 获取玩家余额
-export async function getScore(playerId) {
-  const res = await axios.get(`${API_URL}/api/score/${playerId}`);
-  return res.data;
+const API="https://你的后端Render地址";
+
+
+export function getScore(id){
+
+return axios.get(
+`${API}/api/score/${id}`
+)
+.then(r=>r.data);
+
 }
 
-// 投注
-export async function createBet(data) {
-  const res = await axios.post(`${API_URL}/api/bet`, data);
-  return res.data;
+
+
+export function getBets(id){
+
+return axios.get(
+`${API}/api/bets/${id}`
+)
+.then(r=>r.data);
+
 }
 
-// 获取玩家投注记录
-export async function getBets(playerId) {
-  const res = await axios.get(`${API_URL}/api/bets/${playerId}`);
-  return res.data;
+
+
+
+export function createBet(data){
+
+return axios.post(
+`${API}/api/bet`,
+data
+)
+.then(r=>r.data);
+
 }
 
-// 获取游戏结果
-export async function getGame() {
-  const res = await axios.get(`${API_URL}/api/result`);
-  return res.data;
-}
 
-// ----------------------
-// 后台接口
-export async function getPlayers() {
-  const res = await axios.get(`${API_URL}/admin/players`);
-  return res.data;
-}
 
-export async function updatePlayer(id, data) {
-  const res = await axios.post(`${API_URL}/admin/player/${id}`, data);
-  return res.data;
-}
 
-export async function getRecords() {
-  const res = await axios.get(`${API_URL}/admin/records`);
-  return res.data;
-}
 
-export async function adminOpen(result) {
-  const res = await axios.post(`${API_URL}/admin/open`, { result });
-  return res.data;
-}
+export function getGame(){
 
-export async function adminNext() {
-  const res = await axios.post(`${API_URL}/admin/next`);
-  return res.data;
-}
+return axios.get(
+`${API}/api/game`
+)
+.then(r=>r.data);
 
-export async function createInvite() {
-  const res = await axios.post(`${API_URL}/admin/invite`);
-  return res.data;
 }
